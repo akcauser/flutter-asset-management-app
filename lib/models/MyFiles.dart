a0f0a6a714b247f71387e834a4d377778af4187b
akcauser <akcaertugrulgazi@gmail.com>
2021-05-09 23:07:41 +0300

    initialize

---
+++ lib/models/MyFiles.dart
@@ -0,0 +1,51 @@
+import 'package:admin/constants.dart';
+import 'package:flutter/material.dart';
+
+class CloudStorageInfo {
+  final String svgSrc, title, totalStorage;
+  final int numOfFiels, percentage;
+  final Color color;
+
+  CloudStorageInfo(
+      {this.svgSrc,
+      this.title,
+      this.totalStorage,
+      this.numOfFiels,
+      this.percentage,
+      this.color});
+}
+
+List demoMyFiels = [
+  CloudStorageInfo(
+    title: "All Assets",
+    numOfFiels: 1328,
+    svgSrc: "assets/icons/Documents.svg",
+    totalStorage: "1.9GB",
+    color: primaryColor,
+    percentage: 100,
+  ),
+  CloudStorageInfo(
+    title: "Physical Assets",
+    numOfFiels: 1328,
+    svgSrc: "assets/icons/google_drive.svg",
+    totalStorage: "2.9GB",
+    color: Color(0xFFFFA113),
+    percentage: 35,
+  ),
+  CloudStorageInfo(
+    title: "Digital Assets",
+    numOfFiels: 1328,
+    svgSrc: "assets/icons/one_drive.svg",
+    totalStorage: "1GB",
+    color: Color(0xFFA4CDFF),
+    percentage: 10,
+  ),
+  CloudStorageInfo(
+    title: "Human Assets",
+    numOfFiels: 5328,
+    svgSrc: "assets/icons/drop_box.svg",
+    totalStorage: "7.3GB",
+    color: Color(0xFF007EE5),
+    percentage: 78,
+  ),
+];
