0a36ff376cb64387541a0a9d8ebf5e79416cb1d5
akcauser <akcaertugrulgazi@gmail.com>
2021-05-10 11:05:12 +0300

    firebase test code remove

--- lib/main.dart
+++ lib/main.dart
@@ -8,92 +8,30 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   await Firebase.initializeApp();
-  runApp(App());
+  runApp(MyApp());
 }
 
-class App extends StatefulWidget {
-  // Create the initialization Future outside of `build`:
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  /// The future is part of the state of our widget. We should not call `initializeApp`
-  /// directly inside [build].
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter Admin Error',
-            theme: ThemeData.dark().copyWith(
-              scaffoldBackgroundColor: bgColor,
-              textTheme:
-                  GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-                      .apply(bodyColor: Colors.white),
-              canvasColor: secondaryColor,
-            ),
-            home: MultiProvider(
-              providers: [
-                ChangeNotifierProvider(
-                  create: (context) => MenuController(),
-                ),
-              ],
-              child: MainScreen(),
-            ),
-          );
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter Admin Complete',
-            theme: ThemeData.dark().copyWith(
-              scaffoldBackgroundColor: bgColor,
-              textTheme:
-                  GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-                      .apply(bodyColor: Colors.white),
-              canvasColor: secondaryColor,
-            ),
-            home: MultiProvider(
-              providers: [
-                ChangeNotifierProvider(
-                  create: (context) => MenuController(),
-                ),
-              ],
-              child: MainScreen(),
-            ),
-          );
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Flutter Admin Otherwise',
-          theme: ThemeData.dark().copyWith(
-            scaffoldBackgroundColor: bgColor,
-            textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-                .apply(bodyColor: Colors.white),
-            canvasColor: secondaryColor,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuController(),
           ),
-          home: MultiProvider(
-            providers: [
-              ChangeNotifierProvider(
-                create: (context) => MenuController(),
-              ),
-            ],
-            child: MainScreen(),
-          ),
-        );
-      },
+        ],
+        child: MainScreen(),
+      ),
     );
   }
 }
